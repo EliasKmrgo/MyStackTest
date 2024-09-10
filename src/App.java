e110f89062b64e7a5ed9bf6fc3a6a6ce0eff69dd
Elias <elaiscam1234@gmail.com>
2024-09-09 22:39:02 -0500

    Ajuste en el main

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         JavaReader jr = new JavaReader("src\\JavaFileTest.java");
         jr.showInfo();
+        System.out.println("Fin del archivo");
     }
 }
